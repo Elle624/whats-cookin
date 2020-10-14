9bc4d55a7c3fb7e64433911561607b736fd31509
Elle Li <yiran.li@hotmail.com>
2020-10-14 17:16:31 -0600

    Add pantry class

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,17 +1,43 @@
 class Pantry {
   constructor(pantry) {
-    this.pantry = pantry//[];
+    this.pantry = pantry;
   }
 
-  checkIngredients(recipe) {
-   
-   
+  checkPantry(recipe) {
+    let result = recipe.ingredients.every((ingred) => {
+      return this.pantry.find((eachIngred) => {
+        return eachIngred.ingredient === ingred.id;
+      })
+    })
+    return result;
   }
 
-  removeIngredients(receipe) {
+  checkMissingIngredient(recipe) {
+    let result = recipe.ingredients.filter((ingred) => {
+      return this.pantry.find((eachIngred) => eachIngred.ingredient === ingred.id && eachIngred.acount < ingred.quantity.amount) ||
 
+      !this.pantry.find((eachIngred) => eachIngred.ingredient === ingred.id);
+    })
+    return result
   }
+  // checkMissingIngredient(recipe) {
+  //   let result = recipe.ingredients.filter((ingred) => {
+  //     return !this.pantry.find((eachIngred) => {
+  //       // console.log('recipe', ingred)
+  //         // console.log('pantry', eachIngred)
+  //         // console.log('is true', eachIngred.ingredient !== ingred.id)
+  //         return eachIngred.ingredient === ingred.id
+
+
+  //       // if(eachIngred.ingredient === ingred.id) {
+  //       //   return eachIngred.acount < ingred.quantity.amount;
+  //       // } else if(eachIngred.ingredient !== ingred.id) {
+  //       //   return true;
+  //       // }
+  //     })
+  //   })
+  //   return result;
+  // }
 
 }
-
 module.exports = Pantry;
