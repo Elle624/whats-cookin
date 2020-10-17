4c487a2eddfe9db9902c3f198c9cb2367d38bf4b
Elle Li <yiran.li@hotmail.com>
2020-10-17 15:17:33 -0600

    Refesh home recipes display when return to home page

--- src/scripts.js
+++ src/scripts.js
@@ -20,11 +20,31 @@ homeBtn.addEventListener('click', returnHome);
 toCookBtn.addEventListener('click', viewRecipesToCook);
 filterSection.addEventListener('click', filterByTags);
 myPantryBtn.addEventListener('click', viewMyPantry);
-
+//recipesSection.addEventListener('click', test);
 //gv
 const ingredientsRepo = new IngredientRepo(ingredientsData);
 const recipesRepo = new RecipesRepo(recipeData);
 const user1 = new User(usersData[0]);
+
+// function test() {
+//   let chosenRecipe = 
+//   recipesRepo.recipesArray.find(recipe => recipe.name === event.target.innerText);
+//   recipesSection.innerHTML = '';
+//   let recipeIngredients = chosenRecipe.ingredients.map(recipeIng => {
+//     let ingName = ingredientsRepo.ingredientsArray.find(ing => ing.id === recipeIng.id);
+//     return [ingName.name,recipeIng.quantity.amount + recipeIng.quantity.unit]
+//   })
+  
+//   recipesSection.innerHTML +=
+//   `<article class="recipe-card">
+//     <img src="${chosenRecipe.image}">
+//     <h1 id="recipe-name">${chosenRecipe.name}</h1>
+//     <h1>${chosenRecipe.tags}</h1>
+//     <h2>${recipeIngredients[0]}</h2>
+    
+//   </article>`
+// }
+
 
 function displayRecipes(recipes) {
   recipesSection.innerHTML = ''
@@ -32,7 +52,7 @@ function displayRecipes(recipes) {
     recipesSection.innerHTML +=
   `<article class="recipe-card">
     <img src="${recipeDetail.image}">
-    <h1>${recipeDetail.name}</h1>
+    <h1 id="recipe-name">${recipeDetail.name}</h1>
     <article class="recipe-btns">
       <button class="cook-and-favorite-btn"><svg width="36" height="20" viewBox="0 0 24 24" role="img" aria-hidden="true" tabindex="-1"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path></svg></button>
       <button class="cook-and-favorite-btn">to cook</button>
@@ -83,7 +103,8 @@ function viewFavoriteRecipes() {
 
 function returnHome() {
   let homeSection = [{name: mainPage}, {name: favRecipesPage, add: true}, {name: toCookPage, add: true}, {name: myPantryPage, add: true}];
-  changeClassProperty(homeSection)
+  changeClassProperty(homeSection);
+  displayRecipes(recipesRepo);
 }
 
 function viewRecipesToCook() {
@@ -114,3 +135,4 @@ function displayPantry() {
 }
 
 displayMainPage ();
+
