bc7a090dabba374a4838c6eefab2ecedc515df2c
Elle Li <yiran.li@hotmail.com>
2020-10-17 21:42:39 -0600

    Update returnIds method, and class name

---
+++ src/IngredientsRepo.js
@@ -0,0 +1,29 @@
+//const Ingredient = require("./Ingredient");
+
+class IngredientsRepo {
+  constructor(ingredientDetail = []) {
+    this.ingredientsArray = ingredientDetail.map(detail => new Ingredient(detail.id, detail.name, detail.estimatedCostInCents));
+  }
+
+  calculateRecipeCostByDollar(chosenRecipe) {
+    let cost = chosenRecipe.ingredients.reduce( (totalCost, ingredient) => {
+      let ing = this.ingredientsArray.find(ingred => ingred.id === ingredient.id);
+      totalCost += ing.cost * ingredient.quantity.amount;
+      return totalCost;
+    },0)
+    return cost/100;
+  }
+
+  calculateIngCostByDollar(missIngredient) {
+    let singleCost = this.ingredientsArray.filter(ingredient => ingredient.id === missIngredient.id);
+    return singleCost[0].cost * missIngredient.amount / 100;
+  }
+
+  returnIds(ingredientName) {
+    ingredientName = ingredientName.toLowerCase();
+    let ingredients = this.ingredientsArray.filter(ingre => ingre.name.includes(ingredientName));
+    return ingredients ? ingredients.map(ing => ing.id) : undefined;
+  } 
+
+}
+//module.exports = IngredientRepo;
